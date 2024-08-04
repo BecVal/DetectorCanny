0a3098d2a8f372744db38ad7c89ec7352c2de6ee
Cesar Becerra Valencia <cesarbecval@ciencias.unam.mx>
2024-08-04 11:56:13 -0600

    Modificar el código para ejecutar canny en video

--- canny.py
+++ canny.py
@@ -1,16 +1,19 @@
 #Bibliotecas
 import cv2
 
-#Abrir imagen
-img = cv2.imread('libro.jpg',0)
-
-#Aplicar Canny
-bordeCanny = cv2.Canny(img,100,200)
-
-#Mostrar imagenes
-cv2.imshow('original',img)
-cv2.imshow('blur', bordeCanny)
-
-#Presionar una tecla para dejar de ejecutar
-cv2.waitKey(0)
+capture = cv2.VideoCapture(0)
+while (capture.isOpened()):
+    ret, frame = capture.read()
+    if (ret == True):
+        #Aplicar Canny
+        bordeCanny = cv2.Canny(frame,100,200)
+        #Mostrar imágenes
+        cv2.imshow('Original', frame)
+        cv2.imshow('Canny', bordeCanny)
+        #Presionar una tecla para dejar de ejecutar
+        if (cv2.waitKey(1) == ord('s')):
+            break
+        else:
+            break
+capture.release()
 cv2.destroyAllWindows()
